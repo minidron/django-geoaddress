b2cc528d1471af4cae3f5f05aa0fc9f29f1d433e
Alex Morozov <morozov@podmoskovie.info>
2015-06-19 15:53:57 +0300

    Заменил South на родные миграции.

--- django_geoaddress/migrations/0001_initial.py
+++ django_geoaddress/migrations/0001_initial.py
@@ -1,61 +1,61 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.contrib.gis.db.models.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Country'
-        db.create_table(u'django_geoaddress_country', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
-        ))
-        db.send_create_signal(u'django_geoaddress', ['Country'])
+    dependencies = [
+    ]
 
-        # Adding model 'BaseAddress'
-        db.create_table(u'django_geoaddress_baseaddress', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('country', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['django_geoaddress.Country'])),
-            ('area', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('subarea', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('locality', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('street', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('house', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
-            ('zip', self.gf('django.db.models.fields.CharField')(max_length=10, blank=True)),
-            ('coordinates', self.gf('django.contrib.gis.db.models.fields.PointField')(null=True, blank=True)),
-        ))
-        db.send_create_signal(u'django_geoaddress', ['BaseAddress'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Country'
-        db.delete_table(u'django_geoaddress_country')
-
-        # Deleting model 'BaseAddress'
-        db.delete_table(u'django_geoaddress_baseaddress')
-
-
-    models = {
-        u'django_geoaddress.baseaddress': {
-            'Meta': {'object_name': 'BaseAddress'},
-            'area': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'coordinates': ('django.contrib.gis.db.models.fields.PointField', [], {'null': 'True', 'blank': 'True'}),
-            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['django_geoaddress.Country']"}),
-            'house': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'locality': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'street': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'subarea': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'zip': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'})
-        },
-        u'django_geoaddress.country': {
-            'Meta': {'ordering': "['title']", 'object_name': 'Country'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        }
-    }
-
-    complete_apps = ['django_geoaddress']
+    operations = [
+        migrations.CreateModel(
+            name='BaseAddress',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('area', models.CharField(max_length=255, verbose_name='\u043e\u0431\u043b\u0430\u0441\u0442\u044c', blank=True)),
+                ('subarea', models.CharField(max_length=255, verbose_name='\u0440\u0430\u0439\u043e\u043d', blank=True)),
+                ('locality', models.CharField(max_length=255, verbose_name='\u043d\u0430\u0441\u0435\u043b\u0435\u043d\u043d\u044b\u0439 \u043f\u0443\u043d\u043a\u0442')),
+                ('street', models.CharField(max_length=255, verbose_name='\u0443\u043b\u0438\u0446\u0430', blank=True)),
+                ('house', models.CharField(max_length=50, verbose_name='\u0434\u043e\u043c', blank=True)),
+                ('apartment', models.CharField(max_length=10, verbose_name='\u043e\u0444\u0438\u0441', blank=True)),
+                ('zip', models.CharField(max_length=10, verbose_name='\u043f\u043e\u0447\u0442\u043e\u0432\u044b\u0439 \u0438\u043d\u0434\u0435\u043a\u0441', blank=True)),
+                ('coordinates', django.contrib.gis.db.models.fields.PointField(srid=4326, null=True, verbose_name='\u043a\u043e\u043e\u0440\u0434\u0438\u043d\u0430\u0442\u044b', blank=True)),
+            ],
+            options={
+                'verbose_name': '\u0430\u0434\u0440\u0435\u0441',
+                'verbose_name_plural': '\u0430\u0434\u0440\u0435\u0441\u0430',
+            },
+        ),
+        migrations.CreateModel(
+            name='Country',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255, verbose_name='\u043d\u0430\u0437\u0432\u0430\u043d\u0438\u0435')),
+            ],
+            options={
+                'ordering': ['title'],
+                'verbose_name': '\u0441\u0442\u0440\u0430\u043d\u0430',
+                'verbose_name_plural': '\u0441\u0442\u0440\u0430\u043d\u044b',
+            },
+        ),
+        migrations.CreateModel(
+            name='Region',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name='\u043d\u0430\u0437\u0432\u0430\u043d\u0438\u0435')),
+                ('coordinates', django.contrib.gis.db.models.fields.PolygonField(srid=4326, verbose_name='\u043a\u043e\u043e\u0440\u0434\u0438\u043d\u0430\u0442\u044b')),
+            ],
+            options={
+                'verbose_name': '\u0440\u0435\u0433\u0438\u043e\u043d',
+                'verbose_name_plural': '\u0440\u0435\u0433\u0438\u043e\u043d\u044b',
+            },
+        ),
+        migrations.AddField(
+            model_name='baseaddress',
+            name='country',
+            field=models.ForeignKey(verbose_name='\u0441\u0442\u0440\u0430\u043d\u0430', to='django_geoaddress.Country'),
+        ),
+    ]
